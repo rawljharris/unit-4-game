a7fee8a69ca7a6d65198e22f00012d3b88701453
Rawl harris <rawlharris@yahoo.com>
2019-04-04 15:57:27 -0400

    homework3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,7 @@ function randomNumberGenerator() {
     return Math.floor(Math.random() * 102) + 19;
 }
 console.log(randomResult);
+$("#RandomNum1").text(randomResult);
 
 function randomCrystalGenerated(){
     return Math.floor(Math.random() *12) + 1;
@@ -21,12 +22,27 @@ function randomCrystalGenerated(){
 var crystalOneValue = randomCrystalGenerated();
 var crystalTwoValue = randomCrystalGenerated();
 var crystalThreeValue = randomCrystalGenerated();
-var crystalFourValue = randomCrystalGenerated();
+var crystalFourValue = randomCrystalGenerated(); 
 $("#image1").attr("data-crystalvalue", crystalOneValue);
 $("#image2").attr("data-crystalvalue", crystalTwoValue);
 $("#image3").attr("data-crystalvalue", crystalThreeValue);
 $("#image4").attr("data-crystalvalue", crystalFourValue);
 
+function resetGame() {  
+    randomResult = randomNumberGenerator();
+
+    crystalOneValue = randomCrystalGenerated();
+    crystalTwoValue = randomCrystalGenerated();
+    crystalThreeValue = randomCrystalGenerated();
+    crystalFourValue = randomCrystalGenerated(); 
+    $("#image1").attr("data-crystalvalue", crystalOneValue);
+    $("#image2").attr("data-crystalvalue", crystalTwoValue);
+    $("#image3").attr("data-crystalvalue", crystalThreeValue);
+    $("#image4").attr("data-crystalvalue", crystalFourValue);
+
+    userScore = 0;
+
+}
 
 console.log(crystalOneValue, crystalTwoValue, crystalThreeValue, crystalFourValue);
 
@@ -34,8 +50,33 @@ console.log(crystalOneValue, crystalTwoValue, crystalThreeValue, crystalFourValu
 function incrementScore() {
     // read value from image
     var currentValue = parseInt($(this).attr("data-crystalvalue"))
+
     userScore += currentValue;
     console.log(userScore)
+    
+    //if it is equal, then we increment the win counter
+    if (userScore == randomResult) {
+        win++;
+        console.log("wins:" + win);
+        //print wins to page in wins element
+        $("#wins").text(win);
+        //reset function
+        resetGame();
+        console.log(randomResult);
+        console.log(crystalOneValue, crystalTwoValue, crystalThreeValue, crystalFourValue);
+    }
+    
+    //if greater than the Random result, then they lose add 1 to losses counter
+    if (userScore > randomResult) {
+        loss++;
+        console.log("losses:" + loss);
+        //print wins to page in wins element
+        $("#losses").text(loss);
+        //reset function
+        resetGame();
+        console.log(randomResult);
+        console.log(crystalOneValue, crystalTwoValue, crystalThreeValue, crystalFourValue);
+    }
 }
 
 $("#image1").on("click", incrementScore);
@@ -46,7 +87,3 @@ console.log("#incrementScore")
 
 console.log(userScore);
 
-
-
-//if greater than the Random result, then we decrement a loss counter
-//if it is equal, then we increment a win counter
